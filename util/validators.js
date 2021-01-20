c41e79c1c768ff362716ecd975cb4498516bb5ba
corroonn <corroonn@gmail.com>
2021-01-19 20:28:47 -0500

    Completed context hook and user login

--- util/validators.js
+++ util/validators.js
@@ -15,7 +15,7 @@ module.exports.validateRegisterInput = (
   } else {
     const regEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!email.match(regEx)) {
-      errors.email = "Email mustbe a valid address";
+      errors.email = "Email must be a valid address";
     }
   }
   if (password === "") {
